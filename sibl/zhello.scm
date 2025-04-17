122624d820f1208f151574d71026780626a512a0
4zv4l <4zv4l@protonmail.com>
2025-04-17 20:18:01 +0800

    up: zhello v6!

--- sibl/zhello.scm
+++ sibl/zhello.scm
@@ -8,12 +8,12 @@
 (define-public zhello
   (package
     (name "zhello")
-    (version "0.5")
+    (version "0.6")
     (source
       (origin
         (method url-fetch)
         (uri (string-append "https://github.com/4zv4l/zhello/archive/refs/tags/" version ".tar.gz"))
-        (sha256 (base32 "1v6r0i1p067xsdqypvyl7g4jv1z4gwy64nbxjs3bgzxwjkmn12cv"))))
+        (sha256 (base32 "b7439eca22fd4cfc7af6b6f6dc99a5aaedfceb7ca4ddc828e516328a076dbc94"))))
     (build-system zig-build-system)
     (synopsis "My first guix package using the Zig build system :)")
     (description "Simple hello world in C")
