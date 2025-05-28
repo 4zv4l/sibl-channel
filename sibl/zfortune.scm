64639dd0c67ec20babf01acd4b7a2b1143abcbd7
4zv4l <4zv4l@protonmail.com>
2025-05-28 20:19:38 +0800

    up: change zfortune to release fast

--- sibl/zfortune.scm
+++ sibl/zfortune.scm
@@ -18,7 +18,7 @@
         (sha256 (base32 "1rwr0hsdqzrypc3vm98msfbimyp3z468fgcydzzsfl8r8sdxdbvq"))))
     (build-system zig-build-system)
     (arguments '(#:tests? #f
-                 #:zig-release-type "safe"))
+                 #:zig-release-type "fast"))
     (synopsis "A basic fortune like program in Zig")
     (description "Zfortune is a very basic fortune like program, it just find a random <file>.dat from the env var FORTUNE_PATH and show a random fortune.")
     (home-page "https://github.com/4zv4l/zfortune")
