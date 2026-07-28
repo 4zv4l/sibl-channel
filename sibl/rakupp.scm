077835481f78c11ed97cae83dc023009b4222832
4zv4l <4zv4l@protonmail.com>
2026-07-28 10:18:18 +0800

    fix: closing )

--- sibl/rakupp.scm
+++ sibl/rakupp.scm
@@ -14,7 +14,7 @@
             (method git-fetch)
             (uri (git-reference
                   (url "https://github.com/ash/rakupp")
-                  (commit "v1.2.0"))
+                  (commit "v1.2.0")))
             (file-name (git-file-name name version))
             (sha256
              (base32
